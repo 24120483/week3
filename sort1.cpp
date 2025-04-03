73cceca08fa82bc6f335fc541356fd335fd8f630
24120483 <hoangvantruong080206@gmail.com>
2025-04-03 12:10:54 +0700

    week3

---
+++ sort1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+using namespace  std;
+
+void selection_sort(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		int min = i;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
+	}
+}
+void bubble_sort(int arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+void merge(int arr[], int left, int mid, int right) {
+	int i = left, j = mid + 1, k = 0;
+	int* temp = new int[right - left + 1];
+	while (i <= mid && j <= right) {
+		if (arr[i] <= arr[j]) {
+			temp[k++] = arr[i++];
+		}
+		else {
+			temp[k++] = arr[j++];
+		}
+	}
+	while (i <= mid) {
+		temp[k++] = arr[i++];
+	}
+	while (j <= right) {
+		temp[k++] = arr[j++];
+	}
+	for (int r = 0; r < k; r++) {
+		arr[left + r] = temp[r];
+	}
+	delete[] temp;
+}
+void merge_sort(int arr[], int left, int right) {
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+		merge_sort(arr, left, mid);
+		merge_sort(arr, mid + 1, right);
+		merge(arr, left, mid, right);
+	}
+}
